154ac58679397e015df7204bf5afa2b6abbe4b59
Nate <natjoh29@bergen.org>
2026-05-18 21:02:00 -0400

    week 3 day 1 wrote quarter of the functionality for the api. tomrrow we finish writing the api fully and event listeners.

--- js/api.js
+++ js/api.js
@@ -1,8 +1,23 @@
 function fetchFavs(){
     const query = searchInput.value.trim()
 
-    if(query === "")
-    resultsContainer.innerHTML = '<div class = "empty-state"> You need to put something in the query </div>'
-return
+    if(query === ""){
+        resultsContainer.innerHTML = '<div class = "empty-state"> You need to put something in the query </div>'
+        return
+    }
+    resultsContainer.innerHTML = '<div class = "empty-state"> Loading... </div>'
+    
+    const url = "https://www.thesportsdb.com/api/v1/json/123"
+    
+    fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        currentResults = data
+        displayShows(data);
+    });
 }
-resultsContainer.innerHTML = '<div class = "empty-state"> Loading... </div>'
+
+    
+
--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@
 
 // click event on remove button
 // click event on submit button
-// keydown (enter key) event on search event
+// keydown (enter key) event on search even
+// ========== EVENT LISTENERS ==========
