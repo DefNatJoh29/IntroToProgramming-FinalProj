00c0ad18b2c3ef5598a87d4abf84f208586a6ab4
Nate <natjoh29@bergen.org>
2026-05-23 13:33:09 -0400

    week 3 day 5 part 2, completed the dark mode, and added a horrible color mode, including changes to the js and html

--- js/script.js
+++ js/script.js
@@ -2,17 +2,36 @@
 // toggle dark mode and horrible colors mode
 
 themeBtn = document.querySelector("#theme-toggle")
+badBtn = document.querySelector("#mode-toggle")
 
 function toggleDarkMode(){
+    document.body.classList.remove("bad-mode")
     document.body.classList.toggle("dark-mode")
 
     if (document.body.classList.contains('dark-mode')){
         themeBtn.textContent = "Light Mode"
-    }else {
+    } else {
         themeBtn.textContent = "Dark Mode"
     }
 }
 
 themeBtn.addEventListener("click",(e)=>{
     toggleDarkMode(e)
+})
+
+
+function toggleBadMode(){
+    document.body.classList.remove("dark-mode")
+
+    document.body.classList.toggle("bad-mode")
+
+    if(document.body.classList.contains('bad-mode')){
+        badBtn.textContent = "Good Colors Mode"
+    } else {
+        badBtn.textContent = "Bad Colors Mode"
+    }
+}
+
+badBtn.addEventListener("click",(e)=>{
+    toggleBadMode(e)
 })
