05b0d563b498abf1a9980ae88a7b1c95a07771be
Nate <natjoh29@bergen.org>
2026-05-25 19:34:16 -0400

    Week 4 day 1, fixed positioning on the contact form and fixed other card positionings.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const themeBtn = document.querySelector("#theme-toggle")
 const badBtn = document.querySelector("#mode-toggle")
 const hamburger = document.querySelector(".hamburger")
 const mobileNav = document.querySelector(".mobile-nav")
+const formClass = document.querySelector("#form-class")
+const feedback = document.querySelector(".form-feedback")
 
 function toggleDarkMode(){
     document.body.classList.remove("bad-mode")
@@ -51,4 +53,42 @@ themeBtn.addEventListener("keydown", (e)=>{
 
 hamburger.addEventListener("click",()=>{
     mobileNav.classList.toggle("active")
+})
+
+formClass.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const username = document.querySelector("#username").value.trim()
+    const email = document.querySelector("#email").value.trim()
+    const phone = document.querySelector("#phone").value.trim()
+    const message = document.querySelector("#message").value.trim()
+    
+    if(username.length === 0){
+        feedback.textContent = "❌ Please Enter Your Name!"
+        feedback.style.color = "#E74C3C"
+        return
+    }
+
+    if(!email.includes("@")){
+        feedback.textContent = "❌ Please Enter a Valid Email"
+        feedback.style.color = "#E74C3C"
+        return
+    }
+
+    if(!phone){
+        feedback.textContent = "❌ Please Enter a Phone Number"
+        feedback.style.color = "#E74C3C"
+        return
+    }
+
+    if(message.length < 10){
+        feedback.textContent = "❌ Message must be longer than 10 characters "
+        feedback.style.color = "#E74C3C"
+        return
+    }
+
+
+    feedback.textContent = `✅ Thanks, "${username}! We got your message.`
+    feedback.style.color = "#2ECC71"
+    formClass.reset();
+
 })
