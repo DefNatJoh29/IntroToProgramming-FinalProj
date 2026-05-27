051dcfa16f18096ddca30f7f100eab58a5dcde4b
Nate <natjoh29@bergen.org>
2026-05-27 14:27:48 -0400

    week 4 day 1-3 made a console log js file for debugging purposes. Debugged most of the code, almost completely finished with the project

---
+++ js/console.js
@@ -0,0 +1,9 @@
+// for debugging purposes
+
+document.addEventListener("keydown", (e) => {
+    console.log(e)
+    console.log(`Key: "${e.key}" | Code: "${e.code}" Shift: "${e.shiftKey}"`)
+})
+
+console.log(themeBtn.value.trim())
+
